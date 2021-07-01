7f59ea10cacf359e9d3b0044ca1fe6a0e171aa93
bena2510 <86751971+bena2510@users.noreply.github.com>
2021-07-01 11:18:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Anh nhà ở đâu thế ???
+Cứ tới lui tròn tim tui chẳng nhớ đường về à ☻☻☻`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'oki lun, iu iu em😘😘😘😘 ',
-    btnNo: 'Không, Anh chưa biết được :3',
+    title: 'Người iu anh xinh nhất thế giới đúng hơm :3💁‍♀️💁‍♀️',
+    desc: 'Hí 😛😛',
+    btnYes: 'Tất nhiên rồi, em thứ hai thì ko ai là nhất 🥰🥰🥰  ',
+    btnNo: 'Xinh bình thường :3',
     question:'Thi xong đón em đi chơi nhóa <333',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply: 'Gửi cho em Mai Ngọc Anh ☻',
+    reply: 'Yêu anh thì lúc nào em muốn cũng đc hết á 🥰🥰',
     mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
     messDesc: 'Anh đến đón em xong em lái anh nhá 🙌 .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100020985674960' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
+😍
